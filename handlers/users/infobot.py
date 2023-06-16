883ef2d6e7ff79e09c00a871c9507e403c3708da
SultanO12 <sultanotabekov111@gmail.com>
2023-06-16 18:18:32 +0300

    correction

--- handlers/users/infobot.py
+++ handlers/users/infobot.py
@@ -1,8 +1,14 @@
 from loader import db, dp, bot
 from aiogram import types
 from aiogram.dispatcher import FSMContext
+from keyboards.default.sign import log_markup
 
 @dp.message_handler(text="Bot haqida ℹ️", state='*')
 async def do_info_bot(message: types.Message, state: FSMContext):
     await state.finish()
-    await message.answer("🤖 Bot kotibi o'z serverida saqlanadigan maxfiy ma'lumotlarni yuqori darajada himoya qiladi, u nafaqat matnli ma'lumotlarni, balki voqealar fotosuratlari, ko'rsatmalar va talab qilinishi mumkin bo'lgan boshqa manbalarni ham saqlashi mumkin.\nVa nihoyat, ushbu bot kotibi nafaqat ma'lumotni saqlashda,\nbalki uni boshqarishda ham yordam beradi. ✅")
+    user = await db.select_user(telegram_id=int(message.from_user.id))
+    check_reg = await db.select_reg_user(user_id=int(user['id']))
+    if check_reg:
+        await message.answer("🤖 Bot kotibi o'z serverida saqlanadigan maxfiy ma'lumotlarni yuqori darajada himoya qiladi, u nafaqat matnli ma'lumotlarni, balki voqealar fotosuratlari, ko'rsatmalar va talab qilinishi mumkin bo'lgan boshqa manbalarni ham saqlashi mumkin.\nVa nihoyat, ushbu bot kotibi nafaqat ma'lumotni saqlashda,\nbalki uni boshqarishda ham yordam beradi. ✅")
+    else:
+      await message.answer("Siz hali tizimga kirmagansiz!", reply_markup=log_markup)
--- handlers/users/malumotlar1.py
+++ handlers/users/malumotlar1.py
@@ -139,25 +139,46 @@ async def get_check_del_info(call: types.CallbackQuery, state: FSMContext):
 
 @dp.message_handler(text="➕ Ma'lumot qo'shish")
 async def add_malumot(message: types.Message, state: FSMContext):
-   await message.answer("Qo'shmoqchi bo'lgan narsani tanlang:", reply_markup=add_markup)
+   await state.finish()
+   user = await db.select_user(telegram_id=int(message.from_user.id))
+   check_reg = await db.select_reg_user(user_id=int(user['id']))
+   if check_reg:
+      await message.answer("Qo'shmoqchi bo'lgan narsani tanlang:", reply_markup=add_markup)
+   else:
+      await message.answer("Siz hali tizimga kirmagansiz!", reply_markup=log_markup)
 
 @dp.message_handler(text="➕📝 Matn qo'shish", state='*')
 async def add_text(message: types.Message, state: FSMContext):
    await state.finish()
-   await message.answer("Qo'shmoqchi bo'lgan matnni yuboring:", reply_markup=main_back_markup)
-   await AddMalumot.text.set()
+   user = await db.select_user(telegram_id=int(message.from_user.id))
+   check_reg = await db.select_reg_user(user_id=int(user['id']))
+   if check_reg:
+      await message.answer("Qo'shmoqchi bo'lgan matnni yuboring:", reply_markup=main_back_markup)
+      await AddMalumot.text.set()
+   else:
+      await message.answer("Siz hali tizimga kirmagansiz!", reply_markup=log_markup)
 
 @dp.message_handler(text="➕🖼 Rasm qo'shish", state='*')
 async def add_img(message: types.Message, state: FSMContext):
    await state.finish()
-   await message.answer("Qo'shmoqchi bo'lgan rasimingizni yuboring:", reply_markup=main_back_markup)
-   await AddMalumot.img.set()
+   user = await db.select_user(telegram_id=int(message.from_user.id))
+   check_reg = await db.select_reg_user(user_id=int(user['id']))
+   if check_reg:
+      await message.answer("Qo'shmoqchi bo'lgan rasimingizni yuboring:", reply_markup=main_back_markup)
+      await AddMalumot.img.set()
+   else:
+      await message.answer("Siz hali tizimga kirmagansiz!", reply_markup=log_markup)
 
 @dp.message_handler(text="➕📹 Video qo'shish", state='*')
 async def add_img(message: types.Message, state: FSMContext):
    await state.finish()
-   await message.answer("Qo'shmoqchi bo'lgan videoni yuboring:", reply_markup=main_back_markup)
-   await AddMalumot.video.set()
+   user = await db.select_user(telegram_id=int(message.from_user.id))
+   check_reg = await db.select_reg_user(user_id=int(user['id']))
+   if check_reg:
+      await message.answer("Qo'shmoqchi bo'lgan videoni yuboring:", reply_markup=main_back_markup)
+      await AddMalumot.video.set()
+   else:
+      await message.answer("Siz hali tizimga kirmagansiz!", reply_markup=log_markup)
 
 @dp.message_handler(state=AddMalumot.text)
 async def get_text(message: types.Message, state: FSMContext):
